b7da045e02ab70527b9b0331b3c70998f6b1a816
永超 <491476277@qq.com>
2021-12-19 22:34:15 +0800

    feat: 异常处理

--- .umirc.ts
+++ .umirc.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'bici-components',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: 'http://zysco.test.bicisims.com/static/media/LOGO_WHITE_TEXT.cf0d5096.svg',
+  logo: 'http://zysco.test.bicisims.com/static/media/LOGO_WHITE_TEXT.cf0d5096.svg',
   outputPath: 'docs-dist',
   styles:
     process.env.NODE_ENV === 'development'
       ? [`https://cdn.bootcdn.net/ajax/libs/antd/4.16.9/antd.compact.css`]
-      : [], 
+      : [],
 });
--- src/BiciTable/index.tsx
+++ src/BiciTable/index.tsx
@@ -8,7 +8,9 @@ import React, {
   useCallback,
   useMemo,
 } from 'react';
+import moment from 'moment';
 import {
+  ConfigProvider,
   Table,
   Form,
   Input,
@@ -20,6 +22,9 @@ import {
   Dropdown,
   Menu,
 } from 'antd';
+import ErrorBoundary from '../components/ErrorBoundary';
+import Field from '@ant-design/pro-field';
+
 // import type { ColumnsType } from 'antd';
 
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
@@ -43,195 +48,7 @@ interface BiciTable {
   style?: StyleSheet;
 }
 const PAGE_SIZE = 10;
-const BiciTable = (props: BiciTable, ref: React.Ref<React.ReactNode>) => {
-  const { request, columns } = props;
-  const [tableForm] = Form.useForm();
-  const [selectedRows, setSelectedRows] = useImmer<any[]>([]);
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-  const [dataSource, setDataSource] = useImmer<any[]>([]);
-  const [page, setPage] = useImmer<PageParams>({
-    pageSize: PAGE_SIZE,
-    pageNum: 1,
-  });
 
-  /*
-columns:{
-valueType:'input'
-valueEnum:[]
-    copyable: true,
-     ellipsis: true,
-    tip: '标题过长会自动收缩',
-    formItemProps
-filters: true,
-    onFilter: true,
-    search: false,
-    hideInSearch
-
-}
-
-*/
-
-  const newColumns = [...columns];
-  newColumns.forEach((item) => {
-    if (item.copyable || item.ellipsis) {
-      item.render = (text: any) => {
-        return (
-          <>
-            <Tooltip placement="topLeft" title={text}>
-              <Typography.Paragraph
-                style={{
-                  margin: 0,
-                  padding: 0,
-                }}
-                copyable={item.copyable}
-                ellipsis={item.ellipsis}
-              >
-                {text}
-              </Typography.Paragraph>
-            </Tooltip>
-          </>
-        );
-      };
-    }
-  });
-
-  async function init(pageParams: PageParams = page) {
-    const params: any = await tableForm.getFieldsValue();
-    const res = await request({ ...params, ...pageParams });
-
-    setSelectedRowKeys([]);
-    setDataSource(res?.data);
-    setPage({
-      ...pageParams,
-      pageNum: res?.pageNum,
-      total: res?.total,
-    });
-  }
-
-  useEffect(() => {
-    init(page);
-  }, []);
-  useImperativeHandle(ref, () => {
-    return {
-      init,
-    };
-  });
-  const onSelectChange = (selectedRowKeys: React.Key[], selectedRows: any[]) => {
-    setSelectedRowKeys(selectedRowKeys);
-    setSelectedRows(selectedRows);
-  };
-  const moveRow: any = useCallback(
-    (dragIndex: number, hoverIndex: number) => {
-      setDataSource(
-        produce(dataSource, (e) => {
-          const tmp = e[dragIndex];
-          e[dragIndex] = e[hoverIndex];
-          e[hoverIndex] = tmp;
-        }),
-      );
-    },
-    [dataSource],
-  );
-  const formList = useMemo(() => {
-    return columns?.map((item: any, index: any) => {
-      const { valueType = 'input', valueEnum } = item;
-      const selectTypeEnum = {
-        input: <Input />,
-        select: <Select options={valueEnum} />,
-        dateTime: <DatePicker style={{ width: '100%' }} />,
-        dateRange: <RangePicker />,
-        dateTimeRange: <RangePicker showTime={{ format: 'HH:mm' }} format="YYYY-MM-DD HH:mm" />,
-      };
-      // 屏蔽搜索框
-      if (!item.hideInSearch || item.renderFormItem) {
-        return (
-          <Form.Item name={item.dataIndex} label={item.title} key={index}>
-            {item.renderFormItem
-              ? item.renderFormItem(item)
-              : selectTypeEnum[valueType as 'input' | 'select']}
-          </Form.Item>
-        );
-      }
-    });
-  }, [columns]);
-
-  return (
-    <div>
-      <QueryFilter
-        form={tableForm}
-        onFinish={() => {
-          init(
-            produce(page, (value) => {
-              value.pageNum = 1;
-            }),
-          );
-        }}
-      >
-        {formList}
-      </QueryFilter>
-
-      <div className="flex-between" style={{ padding: '16px 0' }}>
-        <div className={styles.title}>表格</div>
-        <div>
-          <Button style={{ marginRight: 10 }}>新建</Button>
-        </div>
-      </div>
-
-      {selectedRowKeys.length !== 0 && (
-        <div className={styles.selectContainer}>
-          <div>
-            已选择 {selectedRowKeys.length} 项目 &emsp;
-            <a
-              onClick={() => {
-                // onSelectChange()
-                setSelectedRowKeys([]);
-              }}
-            >
-              取消选择
-            </a>
-          </div>
-          <span className={styles.cancelText}>
-            <a href="">批量删除</a>&emsp;
-            <a href="">导出数据</a>
-          </span>
-        </div>
-      )}
-
-      <DndProvider backend={HTML5Backend}>
-        <Table
-          rowSelection={{
-            onChange: onSelectChange,
-            selectedRowKeys,
-          }}
-          rowKey="id"
-          columns={newColumns}
-          dataSource={dataSource}
-          onRow={(record: any, index: any): any => ({
-            index,
-            moveRow,
-          })}
-          components={{
-            body: {
-              row: DraggableBodyRow,
-            },
-          }}
-          pagination={{
-            current: page.pageNum,
-            pageSize: page.pageSize,
-            total: page.total,
-            showSizeChanger: true,
-            pageSizeOptions: ['10', '20', '50', '100'],
-            onChange: (pageNum: number, pageSize: any) => {
-              init({ pageNum, pageSize });
-            },
-          }}
-        />
-      </DndProvider>
-    </div>
-  );
-};
-
-export default forwardRef(BiciTable);
 const type = 'DraggableBodyRow';
 
 const DraggableBodyRow: FC<{
@@ -327,3 +144,210 @@ export const TableDropdown: FC<{
     </Dropdown>
   );
 };
+
+const BiciTable = (props: BiciTable, ref: React.Ref<React.ReactNode>) => {
+  const { request, columns } = props;
+  const [tableForm] = Form.useForm();
+  const [selectedRows, setSelectedRows] = useImmer<any[]>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [dataSource, setDataSource] = useImmer<any[]>([]);
+  const [page, setPage] = useImmer<PageParams>({
+    pageSize: PAGE_SIZE,
+    pageNum: 1,
+  });
+
+  /*
+columns:{
+valueType:'input'
+valueEnum:[]
+    copyable: true,
+     ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps
+filters: true,
+    onFilter: true,
+    search: false,
+    hideInSearch
+
+}
+
+*/
+
+  const newColumns = useMemo(() => {
+    return columns.map((item: any, index: number) => {
+      if (item.copyable || item.ellipsis) {
+        item.render = (text: any) => {
+          return (
+            <>
+              <Tooltip key={index} placement="topLeft" title={text}>
+                <Typography.Paragraph
+                  style={{
+                    margin: 0,
+                    padding: 0,
+                  }}
+                  copyable={item.copyable}
+                  ellipsis={item.ellipsis}
+                >
+                  {text}
+                </Typography.Paragraph>
+              </Tooltip>
+            </>
+          );
+        };
+      }
+      return item;
+    });
+  }, [columns]);
+
+  async function init(pageParams: PageParams = page) {
+    const params: any = await tableForm.getFieldsValue();
+    const res = await request({ ...params, ...pageParams });
+
+    setSelectedRowKeys([]);
+    setDataSource(res?.data);
+    setPage({
+      ...pageParams,
+      pageNum: res?.pageNum,
+      total: res?.total,
+    });
+  }
+
+  useEffect(() => {
+    init(page);
+  }, []);
+  useImperativeHandle(ref, () => {
+    return {
+      init,
+    };
+  });
+  const onSelectChange = (selectedRowKeys: React.Key[], selectedRows: any[]) => {
+    setSelectedRowKeys(selectedRowKeys);
+    setSelectedRows(selectedRows);
+  };
+  const moveRow: any = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      setDataSource(
+        produce(dataSource, (e) => {
+          const tmp = e[dragIndex];
+          e[dragIndex] = e[hoverIndex];
+          e[hoverIndex] = tmp;
+        }),
+      );
+    },
+    [dataSource],
+  );
+  const formList = useMemo(() => {
+    return columns?.map((item: any, index: any) => {
+      const { valueType = 'input', valueEnum } = item;
+      const selectTypeEnum = {
+        input: <Input />,
+        select: <Select options={valueEnum} />,
+        dateTime: <DatePicker style={{ width: '100%' }} />,
+        dateRange: <RangePicker />,
+        dateTimeRange: <RangePicker showTime={{ format: 'HH:mm' }} format="YYYY-MM-DD HH:mm" />,
+      };
+      // 屏蔽搜索框
+      if (!item.hideInSearch || item.renderFormItem) {
+        return (
+          <Form.Item name={item.dataIndex} label={item.title} key={index}>
+            {item.renderFormItem
+              ? item.renderFormItem(item)
+              : selectTypeEnum[valueType as 'input' | 'select']}
+          </Form.Item>
+        );
+      }
+    });
+  }, [columns]);
+  const query = useMemo(() => {
+    return (
+      <QueryFilter
+        form={tableForm}
+        onFinish={() => {
+          init(
+            produce(page, (value) => {
+              value.pageNum = 1;
+            }),
+          );
+        }}
+      >
+        {formList}
+      </QueryFilter>
+    );
+  }, []);
+  const batchContainer = useMemo(() => {
+    return (
+      <>
+        {selectedRowKeys.length !== 0 && (
+          <div className={styles.selectContainer}>
+            <div>
+              已选择 {selectedRowKeys.length} 项目 &emsp;
+              <a
+                onClick={() => {
+                  // onSelectChange()
+                  setSelectedRowKeys([]);
+                }}
+              >
+                取消选择
+              </a>
+            </div>
+            <span className={styles.cancelText}>
+              <a href="">批量删除</a>&emsp;
+              <a href="">导出数据</a>
+            </span>
+          </div>
+        )}
+      </>
+    );
+  }, []);
+
+  return (
+    // <Container.Provider initialState={props}>
+    <ErrorBoundary>
+      <ConfigProvider>
+        <div className="flex-between" style={{ padding: '16px 0' }}>
+          <div className={styles.title}>表格</div>
+          <div>
+            <Button style={{ marginRight: 10 }}>新建</Button>
+          </div>
+        </div>
+
+        {query}
+        {batchContainer}
+
+        <DndProvider backend={HTML5Backend}>
+          <Table
+            rowSelection={{
+              onChange: onSelectChange,
+              selectedRowKeys,
+            }}
+            rowKey="id"
+            columns={newColumns}
+            dataSource={dataSource}
+            onRow={(record: any, index: any): any => ({
+              index,
+              moveRow,
+            })}
+            components={{
+              body: {
+                row: DraggableBodyRow,
+              },
+            }}
+            pagination={{
+              current: page.pageNum,
+              pageSize: page.pageSize,
+              total: page.total,
+              showSizeChanger: true,
+              pageSizeOptions: ['10', '20', '50', '100'],
+              onChange: (pageNum: number, pageSize: any) => {
+                init({ pageNum, pageSize });
+              },
+            }}
+          />
+        </DndProvider>
+      </ConfigProvider>
+    </ErrorBoundary>
+    // </Container.Provider>
+  );
+};
+
+export default forwardRef(BiciTable);
