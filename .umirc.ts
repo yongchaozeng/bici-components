a7d6a58451a57c515d707e2b6154ce5ed050ef00
zhonghe <zhonghe>
2021-12-23 19:07:35 +0800

    feat: logo修改

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'bici-components',
-  favicon: 'http://zysco.test.bicisims.com/static/media/LOGO_WHITE_TEXT.cf0d5096.svg',
-  logo: 'http://zysco.test.bicisims.com/static/media/LOGO_WHITE_TEXT.cf0d5096.svg',
+  favicon: '/logoSVG.svg',
+  logo: '/logoPNG.png',
   outputPath: 'docs-dist',
   styles:
     process.env.NODE_ENV === 'development'
